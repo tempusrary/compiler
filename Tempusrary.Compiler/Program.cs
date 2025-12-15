bb91b0ca56969c7131fba79ac85bb2f972fedfe3
lumentae <66220444+lumentae@users.noreply.github.com>
2025-12-15 19:34:32 +0100

    Add basic compiler interface

--- Tempusrary.Compiler/Program.cs
+++ Tempusrary.Compiler/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.CommandLine;
+using Tempusrary.Compiler.Library.Parsing;
 
-Console.WriteLine("Hello, World!");
+namespace Tempusrary.Compiler;
+
+class Program
+{
+    private static int Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Tempusrary Compiler CLI");
+        var compileCommand = new Command("compile", "Compiles a project.");
+
+        rootCommand.SetAction(_ =>
+        {
+            Console.WriteLine("Use a subcommand, e.g., 'compile'!");
+            return 1;
+        });
+        
+        compileCommand.SetAction(_ =>
+        {
+            Compile();
+        });
+        
+        rootCommand.Add(compileCommand);
+
+        var parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
+    }
+
+    private static void Compile()
+    {
+        var originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        var started = DateTime.Now;
+        Console.WriteLine("Compiling project...");
+        
+        // Iterate files
+        foreach (var file in Directory.EnumerateFiles(".", "*.tpsr", SearchOption.AllDirectories))
+        {
+            var lexer = new Lexer(File.ReadAllText(file));
+            var parser = new Parser(lexer);
+
+            var functions = parser.ParseFunctions();
+        }
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine("Compilation completed.");
+        Console.WriteLine($@"Time elapsed: {(DateTime.Now - started):s\.FFFFFFF}s");
+
+        Console.ForegroundColor = originalColor;
+    }
+}
